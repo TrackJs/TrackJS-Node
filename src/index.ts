8af4d018c6fd5b34ac30c66a13ccfc15bd78ed80
Todd H. Gardner <todd@wocketware.com>
2019-06-13 11:53:48 -0500

    sending simple errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,56 @@
+import https from 'https';
 
 export var TrackJS = {
-  track: function (error: Error): void {
-    return false;
+  track: function (error: Error): any {
+    return new Promise((resolve, reject) => {
+      var req = https.request({
+        method: 'POST',
+        hostname: 'capture.trackjs.com',
+        port: 443,
+        path: '/capture?token=8de4c78a3ec64020ab2ad15dea1ae9ff&v=3.3.0'
+      }, (res) => {
+        var data = ''
+        res.on('data', (chunk) => data += chunk);
+        res.on('end', () => resolve(data || "OK"))
+      })
+      req.on('error', (error) => reject(error))
+      req.write(JSON.stringify({
+        "bindStack": null,
+        "bindTime": null,
+        "console": [],
+        "customer": {
+          "application": "",
+          "correlationId": "1234",
+          "sessionId": "",
+          "token": "8de4c78a3ec64020ab2ad15dea1ae9ff",
+          "userId": "",
+          "version": ""
+        },
+        "entry": "server",
+        "environment": {
+          "age": 0,
+          "dependencies": {
+            "name": "1.0.0"
+          },
+          "originalUrl": "http://example.com/",
+          "referrer": "http://example.com/",
+          "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36",
+          "viewportHeight": 0,
+          "viewportWidth": 0
+        },
+        "file": "",
+        "message": error.message,
+        "metadata": [],
+        "nav": [],
+        "network": [],
+        "url": "http://example.com/",
+        "stack": error.stack,
+        "throttled": 0,
+        "timestamp": new Date().toISOString(),
+        "visitor": [],
+        "version": "3.0.0"
+      }))
+      req.end()
+    })
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,8 +4,9 @@ describe('TrackJS', () => {
 
   describe('track()', () => {
 
-    test('returns undefined', () => {
-      expect(TrackJS.track(new Error('test'))).toBe(undefined);
+    test('returns promise that resolves OK', () => {
+      expect.assertions(1);
+      return TrackJS.track(new Error('node test')).then(data => expect(data).toBe('OK'))
     })
 
   })
