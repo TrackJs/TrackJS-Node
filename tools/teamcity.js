3030e95492c88f8698468eb21273ef2c8f643427
Todd H. Gardner <todd@wocketware.com>
2019-08-06 11:41:45 -0500

    more teamcity.

--- tools/teamcity.js
+++ tools/teamcity.js
@@ -1,4 +1,6 @@
 const package = require("../package.json");
 
 // Magic string for teamcity info.
+// @see https://www.jetbrains.com/help/teamcity/build-script-interaction-with-teamcity.html
 console.log('##teamcity[buildNumber \'' + package.version + '-build.{build.number}\']');
+console.log('##teamcity[packageVersion \'' + package.version + '\']');
